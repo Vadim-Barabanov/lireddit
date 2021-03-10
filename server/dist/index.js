778f2d44f1ee6d4f38127396e6a7a3b70fd27136
Vadim Barabanov <introvert.png@gmail.com>
2021-03-10 23:38:04 +0200

    MikroORM base migrations created

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,13 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
-    const post = orm.em.create(Post_1.Post, { title: 'My first post' });
-    yield orm.em.persistAndFlush(post);
+    yield orm.getMigrator().up();
 });
 main().catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
@@ -5,9 +6,13 @@ import mikroConfig from './mikro-orm.config'
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig)
+    await orm.getMigrator().up()
 
-    const post = orm.em.create(Post, { title: 'My first post' })
-    await orm.em.persistAndFlush(post)
+    // const post = orm.em.create(Post, { title: 'My first post' })
+    // await orm.em.persistAndFlush(post)
+
+    // const posts = await orm.em.find(Post, {})
+    // console.log(posts)
 }
 
 main().catch((err) => console.log(err))
